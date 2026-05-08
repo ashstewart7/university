631ef6274ea5baec298dc5425c24ab43adf186ed
Ash Stewart <ashstewart7@proton.me>
2026-05-08 22:47:38 +0100

    A tonne of various bits

--- scripts/config.py
+++ scripts/config.py
@@ -3,5 +3,5 @@ from pathlib import Path
 CURRENT_MODULE_SYMLINK = Path('~/Documents/uni/source_notes/active_module').expanduser()
 CURRENT_MODULE_ROOT = CURRENT_MODULE_SYMLINK.resolve()
 CURRENT_MODULE_WATCH_FILE = Path('/tmp/current_module').resolve()
-ROOT = Path('/home/ash/Documents/uni/source_notes/Year 1 Semester 1').expanduser()
+ROOT = Path('/home/ash/Documents/uni/source_notes/Year 1 Semester 2').expanduser()
 DATE_FORMAT = '%a %d %b %Y %H:%M'
